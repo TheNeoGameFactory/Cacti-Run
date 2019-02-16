7de66f2b33b30829e626af23f4b05bb08189ae41
Christoph Winkler <support@neo-gamefactory.de>
2019-02-16 21:14:16 +0100

    Trigger

--- game/Scripts/TriggerPlayAnimation.gd
+++ game/Scripts/TriggerPlayAnimation.gd
@@ -4,6 +4,10 @@ export (NodePath) var animationPlayer
 export (String) var startAnimationName
 
 
+
+func _ready():
+	self.connect("area_entered",self,"_on_Area_body_entered")
+	
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		get_node(animationPlayer).play(startAnimationName)
